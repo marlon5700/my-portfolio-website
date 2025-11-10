6073d6b4ca04271d5e4dad26afe2c810e5ff6171
Marlon5700 <marlonthompson5700@gmail.com>
2025-11-10 00:22:55 +0000

    Website Update.
    Completed the main layout of the website.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const video1 = document.getElementById('projectVideo1');
 
 const videoList = [video1];
 
+const navbar = document.getElementById('top-nav')
+
 videoList.forEach(function(video){
     video.addEventListener('mouseover', function(){
         video.play()
@@ -11,3 +13,19 @@ videoList.forEach(function(video){
         video.pause()
     });
 });
+
+
+const menu = document.querySelector('#mobile-menu')
+
+const menuLinks = document.querySelector('.navbar__menu')
+
+const navBar = document.getElementById('top-nav')
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    navBar.classList.toggle('active')
+
+    
+      
+})
